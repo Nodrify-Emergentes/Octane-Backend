e7e2f53ded1284694cecd18cb8642e347b0d6992
Pachie <54517063+Pachieeee@users.noreply.github.com>
2025-11-30 11:43:50 -0500

    feat(devices): implement device registration and validation commands.

--- src/main/java/nrg/inc/bykerz/iam/application/internal/commandservices/DeviceCommandServiceImpl.java
+++ src/main/java/nrg/inc/bykerz/iam/application/internal/commandservices/DeviceCommandServiceImpl.java
@@ -1,29 +1,46 @@
 package nrg.inc.bykerz.iam.application.internal.commandservices;
 
+import nrg.inc.bykerz.iam.application.internal.outboundservices.tokens.TokenService;
 import nrg.inc.bykerz.iam.domain.model.aggregates.Device;
 import nrg.inc.bykerz.iam.domain.model.commands.RegisterDeviceCommand;
 import nrg.inc.bykerz.iam.domain.model.commands.ValidateDeviceCommand;
 import nrg.inc.bykerz.iam.domain.services.DeviceCommandService;
 import nrg.inc.bykerz.iam.infrastructure.persistence.jpa.repositories.DeviceRepository;
 import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public class DeviceCommandServiceImpl implements DeviceCommandService {
 
     private final DeviceRepository deviceRepository;
+    private final TokenService tokenService;
 
-    public DeviceCommandServiceImpl(DeviceRepository deviceRepository) {
+    public DeviceCommandServiceImpl(DeviceRepository deviceRepository, TokenService tokenService) {
         this.deviceRepository = deviceRepository;
+        this.tokenService = tokenService;
     }
 
     @Override
     public Optional<Device> handle(RegisterDeviceCommand registerDeviceCommand) {
-        return Optional.empty();
+        if (deviceRepository.existsById(registerDeviceCommand.deviceId())) {
+            throw new IllegalArgumentException("Device already exists");
+        }
+        var device = new Device(registerDeviceCommand.deviceId(), 0L);
+        deviceRepository.save(device);
+        return Optional.of(device);
     }
 
     @Override
     public Optional<ImmutablePair<Device, String>> handle(ValidateDeviceCommand validateDeviceCommand) {
-        return Optional.empty();
+        var device = deviceRepository.findById(validateDeviceCommand.deviceId());
+
+        if (device.isEmpty()) {
+            throw new IllegalArgumentException("Device does not exist");
+        }
+        var token = tokenService.generateToken(String.valueOf(device.get().getId()));
+
+        return Optional.of(ImmutablePair.of(device.get(), token));
     }
 }
