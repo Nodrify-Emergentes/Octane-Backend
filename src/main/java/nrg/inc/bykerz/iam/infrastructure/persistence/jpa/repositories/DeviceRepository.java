7b85378c4003fa635c8dfda9bb1fcfbbee00c5e3
Pachie <54517063+Pachieeee@users.noreply.github.com>
2025-11-30 11:44:06 -0500

    feat(devices): add method to find device by vehicle ID.

--- src/main/java/nrg/inc/bykerz/iam/infrastructure/persistence/jpa/repositories/DeviceRepository.java
+++ src/main/java/nrg/inc/bykerz/iam/infrastructure/persistence/jpa/repositories/DeviceRepository.java
@@ -3,5 +3,8 @@ package nrg.inc.bykerz.iam.infrastructure.persistence.jpa.repositories;
 import nrg.inc.bykerz.iam.domain.model.aggregates.Device;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface DeviceRepository extends JpaRepository<Device, String> {
+    Optional<Device> findByVehicleId(String vehicleId);
 }
