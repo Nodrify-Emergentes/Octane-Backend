b17af5fae8511ad6a521cb27c5b05f3195a656c9
Pachie <54517063+Pachieeee@users.noreply.github.com>
2025-11-30 11:42:42 -0500

    feat(devices): enhance device model with JPA annotations and constructors.

--- src/main/java/nrg/inc/bykerz/iam/domain/model/aggregates/Device.java
+++ src/main/java/nrg/inc/bykerz/iam/domain/model/aggregates/Device.java
@@ -1,8 +1,23 @@
 package nrg.inc.bykerz.iam.domain.model.aggregates;
 
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
 import nrg.inc.bykerz.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
 
+@Setter
+@Getter
+@Entity
 public class Device extends AuditableAbstractAggregateRoot<Device> {
     private String deviceId;
-    private String vehicleId;
+    private Long vehicleId;
+
+    public Device() {
+        super();
+    }
+
+    public Device(String deviceId, Long vehicleId) {
+        this.deviceId = deviceId;
+        this.vehicleId = vehicleId;
+    }
 }
