c62f397e054ed6f90a3b6c4c4052edb524afebcc
Pachie <54517063+Pachieeee@users.noreply.github.com>
2025-11-30 15:21:31 -0500

    feat(devices): integrate external vehicle service for vehicle existence validation during device registration.

--- src/main/java/nrg/inc/bykerz/iam/application/internal/commandservices/DeviceCommandServiceImpl.java
+++ src/main/java/nrg/inc/bykerz/iam/application/internal/commandservices/DeviceCommandServiceImpl.java
@@ -6,6 +6,7 @@ import nrg.inc.bykerz.iam.domain.model.commands.RegisterDeviceCommand;
 import nrg.inc.bykerz.iam.domain.model.commands.ValidateDeviceCommand;
 import nrg.inc.bykerz.iam.domain.services.DeviceCommandService;
 import nrg.inc.bykerz.iam.infrastructure.persistence.jpa.repositories.DeviceRepository;
+import nrg.inc.bykerz.shared.application.internal.outboundservices.acl.ExternalVehiclesService;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.springframework.stereotype.Service;
 
@@ -15,19 +16,26 @@ import java.util.Optional;
 public class DeviceCommandServiceImpl implements DeviceCommandService {
 
     private final DeviceRepository deviceRepository;
+    private final ExternalVehiclesService externalVehiclesService;
     private final TokenService tokenService;
 
-    public DeviceCommandServiceImpl(DeviceRepository deviceRepository, TokenService tokenService) {
+    public DeviceCommandServiceImpl(DeviceRepository deviceRepository, ExternalVehiclesService externalVehiclesService, TokenService tokenService) {
         this.deviceRepository = deviceRepository;
+        this.externalVehiclesService = externalVehiclesService;
         this.tokenService = tokenService;
     }
 
     @Override
     public Optional<Device> handle(RegisterDeviceCommand registerDeviceCommand) {
-
         if (deviceRepository.existsByDeviceId(registerDeviceCommand.deviceId())) {
             throw new IllegalArgumentException("Device already exists");
         }
+
+        var vehicle = externalVehiclesService.fetchVehicleById(registerDeviceCommand.vehicleId());
+        if (vehicle.isEmpty()) {
+            throw new IllegalArgumentException("Vehicle does not exist");
+        }
+
         var device = new Device(registerDeviceCommand.deviceId(), registerDeviceCommand.vehicleId());
         deviceRepository.save(device);
         return Optional.of(device);
