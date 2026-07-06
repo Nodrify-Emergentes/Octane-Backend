14b40bb35b77cb379aa27a6a77584ebb9ee1cb20
SebastianPacheco <u202110291@upc.edu.pe>
2026-07-06 12:10:20 -0500

    feat: add generative AI service for generating summaries using AI model.

---
+++ src/main/java/nodrify/inc/octane/wellness/domain/services/GenerativeAIService.java
@@ -0,0 +1,31 @@
+package nodrify.inc.octane.wellness.domain.services;
+
+import com.google.genai.Client;
+import com.google.genai.types.GenerateContentResponse;
+import nodrify.inc.octane.wellness.application.internal.outboundservices.ai.GenerativeAIClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GenerativeAIService {
+    private final Client client;
+
+    @Value("${ai.summary.model}")
+    private String model;
+
+
+    public GenerativeAIService(GenerativeAIClient client) {
+        this.client = client.configure();
+    }
+
+    public String generateSummary(String prompt) {
+        GenerateContentResponse response =
+                client.models.generateContent(
+                        model,
+                        prompt,
+                        null);
+
+        // Extract text from the SDK response
+        return response.text();
+    }
+}
